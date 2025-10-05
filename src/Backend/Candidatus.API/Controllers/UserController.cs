32af3b4b0182d34c750cfdb9c3b97d05a1ea0fd7
Mateus Leonardo <mateus_leonardo1997@hotmail.com>
2025-10-05 11:47:01 -0300

    fixing coding styles

--- src/Backend/Candidatus.API/Controllers/UserController.cs
+++ src/Backend/Candidatus.API/Controllers/UserController.cs
@@ -10,8 +10,7 @@ public class UserController : CandidatusBaseController
     [HttpPost]
     [ProducesResponseType(typeof(ResponseRegisteredUserJson), StatusCodes.Status201Created)]
     [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
-    public async Task<IActionResult> Register(
-        [FromBody] RequestRegisterUserJson request,
+    public async Task<IActionResult> Register([FromBody] RequestRegisterUserJson request,
         [FromServices] IRegisterUserUseCase useCase)
     {
         var response = await useCase.Execute(request);
--- src/Backend/Candidatus.Application/UseCases/User/Register/RegisterUserUseCase.cs
+++ src/Backend/Candidatus.Application/UseCases/User/Register/RegisterUserUseCase.cs
@@ -5,16 +5,19 @@ using Candidatus.Domain.Repositories.User;
 using Candidatus.Domain.Security.Cryptography;
 using Candidatus.Exceptions;
 using Candidatus.Exceptions.ExceptionsBase;
+using FluentValidation.Results;
 using MapsterMapper;
 
 namespace Candidatus.Application.UseCases.User.Register;
+
 public class RegisterUserUseCase : IRegisterUserUseCase
 {
-    private readonly IUserWriteOnlyRepository _userWriteOnlyRepository;
-    private readonly IUserReadOnlyRepository _userReadOnlyRepository;
-    private readonly IUnitOfWork _unitOfWork;
     private readonly IMapper _mapper;
     private readonly IPasswordEncripter _passwordEncripter;
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IUserReadOnlyRepository _userReadOnlyRepository;
+    private readonly IUserWriteOnlyRepository _userWriteOnlyRepository;
+
     public RegisterUserUseCase(
         IUserWriteOnlyRepository writeOnlyRepository,
         IUserReadOnlyRepository readOnlyRepository,
@@ -41,7 +44,6 @@ public class RegisterUserUseCase : IRegisterUserUseCase
         await _unitOfWork.CommitAsync();
 
         return _mapper.Map<ResponseRegisteredUserJson>(user);
-
     }
 
     private async Task Validate(RequestRegisterUserJson request)
@@ -51,11 +53,9 @@ public class RegisterUserUseCase : IRegisterUserUseCase
         var user = await _userReadOnlyRepository.GetByEmail(request.Email);
 
         if (user is not null)
-            result.Errors.Add(new FluentValidation.Results.ValidationFailure(string.Empty, ResourceMessagesExceptions.EMAIL_EXISTS));
-
+            result.Errors.Add(new ValidationFailure(string.Empty, ResourceMessagesExceptions.EMAIL_EXISTS));
 
         if (!result.IsValid)
             throw new ErrorOnValidationException(result.Errors.Select(error => error.ErrorMessage).ToList());
-
     }
 }
