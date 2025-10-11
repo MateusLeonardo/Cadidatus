0d5bfa351f6928c83057d399835489c5b0d1119c
Mateus Leonardo <mateus_leonardo1997@hotmail.com>
2025-10-11 11:13:10 -0300

    adding password validator

---
+++ src/Backend/Candidatus.Application/SharedValidators/PasswordValidator.cs
@@ -0,0 +1,32 @@
+﻿using Candidatus.Exceptions;
+using FluentValidation;
+using FluentValidation.Validators;
+
+namespace Candidatus.Application.SharedValidators;
+
+public class PasswordValidator<T> : PropertyValidator<T, string>
+{
+    public override string Name => "PasswordValidator";
+
+    public override bool IsValid(ValidationContext<T> context, string password)
+    {
+        if (string.IsNullOrWhiteSpace(password))
+        {
+            context.MessageFormatter.AppendArgument("ErrorMessage", ResourceMessagesExceptions.PASSWORD_EMPTY);
+            return false;
+        }
+
+        if (password.Length < 6)
+        {
+            context.MessageFormatter.AppendArgument("ErrorMessage", ResourceMessagesExceptions.PASSWORD_MIN_LENGTH);
+            return false;
+        }
+
+        return true;
+    }
+
+    protected override string GetDefaultMessageTemplate(string errorCode)
+    {
+        return "{ErrorMessage}";
+    }
+}
