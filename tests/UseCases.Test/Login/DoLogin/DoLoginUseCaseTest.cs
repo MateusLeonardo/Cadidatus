f2233c51e6cc2228f619ea237016c89710db2b96
Mateus Leonardo <mateus_leonardo1997@hotmail.com>
2025-10-11 20:43:39 -0300

    adding test for login use case

---
+++ tests/UseCases.Test/Login/DoLogin/DoLoginUseCaseTest.cs
@@ -0,0 +1,56 @@
+﻿using Candidatus.Application.UseCases.Login.DoLogin;
+using Candidatus.Communication.Requests;
+using Candidatus.Exceptions;
+using Candidatus.Exceptions.ExceptionsBase;
+using CommonTestUtilities.Cryptography;
+using CommonTestUtilities.Entities;
+using CommonTestUtilities.Repositories;
+using CommonTestUtilities.Requests;
+using CommonTestUtilities.Tokens;
+using FluentAssertions;
+
+namespace UseCases.Test.Login.DoLogin;
+
+public class DoLoginUseCaseTest
+{
+    [Fact]
+    public async Task Success()
+    {
+        var (user, password) = UserBuilder.Build();
+        var useCase = CreateUseCase(user);
+
+        var result = await useCase.Execute(new RequestLoginJson
+        {
+            Email = user.Email,
+            Password = password
+        });
+
+        result.Should().NotBeNull();
+        result.Tokens.AccessToken.Should().NotBeNullOrEmpty();
+    }
+
+    [Fact]
+    public async Task Error_Invalid_User()
+    {
+        var (user, password) = UserBuilder.Build();
+        var request = RequestLoginJsonBuilder.Build();
+        var useCase = CreateUseCase();
+
+        var act = async () => await useCase.Execute(request);
+
+        await act.Should().ThrowAsync<InvalidLoginException>()
+            .Where(e => e.Message.Equals(ResourceMessagesExceptions.EMAIL_OR_PASSWORD_INVALID));
+    }
+
+    private static DoLoginUseCase CreateUseCase(Candidatus.Domain.Entities.User? user = null)
+    {
+        var readOnlyRepository = new UserReadOnlyRepositoryBuilder();
+        var accessToken = JwtTokenGeneratorBuilder.Buid();
+        var passwordEncripter = PasswordEncripterBuilder.Build();
+
+        if (user is not null)
+            readOnlyRepository.GetByEmail(user);
+
+        return new DoLoginUseCase(readOnlyRepository.Build(), accessToken, passwordEncripter);
+    }
+}
