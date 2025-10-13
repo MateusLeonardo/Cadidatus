9023d19d41046978cc759233021a95ce66b9f3b3
Mateus Leonardo <mateus_leonardo1997@hotmail.com>
2025-10-13 20:54:05 -0300

    Add unit tests for ChangePasswordValidator with password validation scenarios

---
+++ tests/CommonTestUtilities/Requests/RequestChangePasswordBuilder.cs
@@ -0,0 +1,13 @@
+﻿using Bogus;
+using Candidatus.Communication.Requests;
+
+namespace CommonTestUtilities.Requests;
+public class RequestChangePasswordBuilder
+{
+    public static RequestChangePasswordUserJson Build(int passwordLenght = 10)
+    {
+        return new Faker<RequestChangePasswordUserJson>()
+            .RuleFor(r => r.Password, (f) => f.Internet.Password())
+            .RuleFor(r => r.NewPassword, (f) => f.Internet.Password(passwordLenght));
+    }
+}
---
+++ tests/Validators.Test/User/ChangePassword/ChangePasswordValidatorTest.cs
@@ -0,0 +1,51 @@
+﻿using Candidatus.Application.UseCases.User.ChangePassword;
+using Candidatus.Exceptions;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+
+namespace Validators.Test.User.ChangePassword;
+public class ChangePasswordValidatorTest
+{
+    [Fact]
+    public void Success()
+    {
+        var request = RequestChangePasswordBuilder.Build();
+        var result = new ChangePasswordUserValidator().Validate(request);
+
+        result.IsValid.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Error_Password_Empty()
+    {
+        var request = RequestChangePasswordBuilder.Build();
+        request.NewPassword = string.Empty;
+
+        var validator = new ChangePasswordUserValidator();
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And
+            .Contain(e => e.ErrorMessage.Equals(ResourceMessagesExceptions.PASSWORD_EMPTY));
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+    [InlineData(4)]
+    [InlineData(5)]
+    public void Error_Password_Min_Lenght(int passwordLenght)
+    {
+        var request = RequestChangePasswordBuilder.Build(passwordLenght);
+
+        var validator = new ChangePasswordUserValidator();
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And
+            .Contain(e => e.ErrorMessage.Equals(ResourceMessagesExceptions.PASSWORD_MIN_LENGTH));
+    }
+}
