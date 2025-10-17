a6efc8cd722de2deba4be61847e75eb3780bd51a
Mateus Leonardo <mateus_leonardo1997@hotmail.com>
2025-10-16 21:25:17 -0300

    adding state entity

---
+++ src/Backend/Candidatus.Domain/Entities/State.cs
@@ -0,0 +1,6 @@
+﻿namespace Candidatus.Domain.Entities;
+public class State : EntityBase
+{
+    public string Name { get; set; } = string.Empty;
+    public string Uf { get; set; } = string.Empty;
+}
