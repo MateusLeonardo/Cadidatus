0c5ff6452b0ab43c1a6aea7603d96096e50e0c9f
Mateus Leonardo <mateus_leonardo1997@hotmail.com>
2025-10-18 14:39:22 -0300

    creating register state integration test with token invalid

---
+++ tests/WebApi.Test/State/Register/RegisterStateInvalidTokenTest.cs
@@ -0,0 +1,22 @@
+﻿using CommonTestUtilities.Requests;
+using FluentAssertions;
+using System.Net;
+
+namespace WebApi.Test.State.Register;
+public class RegisterStateInvalidTokenTest : CandidatusClassFixture
+{
+    private readonly string METHOD = "state";
+    public RegisterStateInvalidTokenTest(CustomWebApplicationFactory factory) : base(factory)
+    {
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        var request = RequestRegisterStateJsonBuilder.Build();
+
+        var response = await DoPost(METHOD, request, "invalidToken");
+
+        response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+    }
+}
