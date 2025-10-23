99c473d9eefbc4b715aee37e84350d1a079aee6a
Mateus Leonardo <mateus_leonardo1997@hotmail.com>
2025-10-22 22:51:05 -0300

    feat: adding test for state update validator

---
+++ tests/CommonTestUtilities/Requests/RequestUpdateStateJsonBuilder.cs
@@ -0,0 +1,20 @@
+﻿using Bogus;
+using Candidatus.Communication.Requests;
+
+namespace CommonTestUtilities.Requests;
+public class RequestUpdateStateJsonBuilder
+{
+    public static RequestUpdateStateJson Build()
+    {
+        return new Faker<RequestUpdateStateJson>()
+            .RuleFor(r => r.Name, (f) => f.Address.State())
+            .RuleFor(r => r.Uf, (f) => f.Address.StateAbbr());
+    }
+
+    public static RequestUpdateStateJson BuildWithUfLength(int length)
+    {
+        return new Faker<RequestUpdateStateJson>()
+            .RuleFor(r => r.Name, (f) => f.Address.State())
+            .RuleFor(r => r.Uf, (f) => f.Random.String(length));
+    }
+}
