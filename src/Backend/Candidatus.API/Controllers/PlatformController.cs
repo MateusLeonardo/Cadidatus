c1fd3909277bbd36dd6a936853402ff339a2b832
Mateus Leonardo <mateus_leonardo1997@hotmail.com>
2025-11-20 21:32:32 -0300

    feat: implement PlatformController for handling platform registration requests

---
+++ src/Backend/Candidatus.API/Controllers/PlatformController.cs
@@ -0,0 +1,21 @@
+using Candidatus.API.Attributes;
+using Candidatus.Application.UseCases.Platform.Register;
+using Candidatus.Communication.Requests;
+using Candidatus.Communication.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Candidatus.API.Controllers;
+
+[AuthenticatedUser]
+public class PlatformController : CandidatusBaseController
+{
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Register([FromBody] RequestRegisterPlatformJson request,
+        [FromServices] IRegisterPlatformUseCase useCase)
+    {
+        await useCase.Execute(request);
+        return Created(string.Empty, null);
+    }
+}
