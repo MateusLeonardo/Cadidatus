52acfe04cc6f0129fe376e683257bf4f7215e397
Mateus Leonardo <mateus_leonardo1997@hotmail.com>
2025-11-20 21:29:53 -0300

    feat: add RequestRegisterPlatformJson class for platform registration requests

---
+++ src/Shared/Candidatus.Communication/Requests/RequestRegisterPlatformJson.cs
@@ -0,0 +1,7 @@
+namespace Candidatus.Communication.Requests;
+
+public class RequestRegisterPlatformJson
+{
+    public string Name { get; set; } = string.Empty;
+    public string Url { get; set; } = string.Empty;
+}
