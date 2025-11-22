3b768482e6f4928caf374569ea906a8e5c49b67c
Mateus Leonardo <mateus_leonardo1997@hotmail.com>
2025-11-22 19:15:46 -0300

    test: add CompanyReadOnlyRepositoryBuilder for mocking CompanyReadOnlyRepository in tests

---
+++ tests/CommonTestUtilities/Repositories/Company/CompanyReadOnlyRepositoryBuilder.cs
@@ -0,0 +1,14 @@
+using Candidatus.Domain.Repositories.Company;
+using Moq;
+
+namespace CommonTestUtilities.Repositories.Company;
+
+public class CompanyReadOnlyRepositoryBuilder
+{
+    private readonly Mock<ICompanyReadOnlyRepository> _repository = new();
+
+    public void FindById(Candidatus.Domain.Entities.Company company, Candidatus.Domain.Entities.User user)
+    {
+        _repository.Setup(repo => repo.FindById(company.Id, user)).ReturnsAsync(company);
+    }
+}
